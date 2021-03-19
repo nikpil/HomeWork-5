b7dce38e178acaae6d8c5f059b78571f6db279db
Nikpil <pelipenkonikolay@gmail.com>
2021-03-19 18:43:14 +0300

    Выполнена вторая часть задания.

--- src/main/java/par1/InfoPrint.java
+++ src/main/java/par1/InfoPrint.java
@@ -1,10 +1,37 @@
 package par1;
 
-public class InfoPrint
-{
+//import java.util.Arrays;
+
+public class InfoPrint {
     public static void main(String[] args) {
-        Worker user = new Worker("Иванов Иван Иваныч. ","email@mail.ru ", "Страшный специалист. ",
-                "+7123456789 ",30 , "За еду. ");
+        // Первая часть задания
+        Worker user = new Worker("Иванов Иван Иваныч. ", "email@mail.ru ", "Страшный специалист. ",
+                "+7123456789 ", 30, 30000);
         user.printWorker();
+
+
+        // Вторая часть задания
+        Worker[] arr = new Worker[5];
+        arr[0] = new Worker("Сидоров Иван. ","email@mail.ru ", "Страшный специалист. ",
+                "+7123456789 ",50 , 80000);
+        arr[1] = new Worker("Смирнов Николай Алексеевич. ","email@mail.ru ", "Специалист. ",
+                "+7123456789 ",41 , 54000);
+        arr[2] = new Worker("Иванов Иван Иваныч. ","email@mail.ru ", "Не специалист. ",
+                "+7123456789 ",30 , 33000);
+        arr[3] = new Worker("Кочетов Владимир Сергеевич. ","email@mail.ru ", "Специалист. ",
+                "+7123456789 ",36 , 47000);
+        arr[4] = new Worker("Петушков Сергей Владимирович. ","email@mail.ru ", "Очень специалист. ",
+                "+7123456789 ",56 , 60000);
+
+        for (int i=0;i<arr.length; i++){
+            if (arr[i].age> 40){
+                arr[i].printWorker();
+            }
+        }
     }
+
 }
+
+
+
+
